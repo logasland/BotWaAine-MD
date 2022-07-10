db5ebe513f8bda5d5e3e3beb2169fd32fc583510
A̐̈͢͢͢r̐̈͢͢͢d̐̈͢͢͢h̐̈͢͢͢i̐̈͢͢͢X̐̈͢͢͢s̐̈͢͢͢ <77874962+ArdhiXs@users.noreply.github.com>
2022-07-10 20:12:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6288295249563'],
+  ['6288294052009'],
+  ['6288294052009', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -40,8 +40,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = '@ainee_bot'
-  var sticker_author = 'Aine'
+  var sticker_name = 'aXsBot-MD'
+  var sticker_author = '\n\n\n\n\n @ardhixs_'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
@@ -58,7 +58,7 @@ fs.watchFile(file_exif, () => {
 // Sticker WM
 global.packname = sticker_name
 global.author = sticker_author
-global.wm = '© Aine'
+global.wm = '©ardhixs_'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
